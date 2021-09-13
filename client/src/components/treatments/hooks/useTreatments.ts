15fb01da89adc0f1a15a46e602f919df737433f9
Cloudbop <boppincolin@gmail.com>
2021-09-13 23:18:02 +0100

    +default Error toast

--- client/src/components/treatments/hooks/useTreatments.ts
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -1,10 +1,8 @@
-import { useToast } from '@chakra-ui/react';
 import { useQuery } from 'react-query';
 
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -14,19 +12,7 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   const fallback = [];
-  const toast = useCustomToast();
   // TODO: get data from server via useQuery
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
-    onError: (error) => {
-      const title =
-        // is javascript Error
-        error instanceof Error
-          ? // Regex remove
-            error.toString().replace(/^Error:\s*/, '')
-          : //
-            'error connecting to the server';
-      toast({ title, status: 'error' });
-    },
-  });
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
   return data;
 }
