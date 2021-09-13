4249e805a5b69009cbef7af7e3a181723e68a257
Cloudbop <boppincolin@gmail.com>
2021-09-13 23:09:01 +0100

    connect queryError to useToast

--- client/src/components/treatments/hooks/useTreatments.ts
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -1,3 +1,4 @@
+import { useToast } from '@chakra-ui/react';
 import { useQuery } from 'react-query';
 
 import type { Treatment } from '../../../../../shared/types';
@@ -13,7 +14,19 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   const fallback = [];
+  const toast = useCustomToast();
   // TODO: get data from server via useQuery
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+    onError: (error) => {
+      const title =
+        // is javascript Error
+        error instanceof Error
+          ? // Regex remove
+            error.toString().replace(/^Error:\s*/, '')
+          : //
+            'error connecting to the server';
+      toast({ title, status: 'error' });
+    },
+  });
   return data;
 }
