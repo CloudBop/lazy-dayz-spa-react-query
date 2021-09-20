58d3cc66384c58a104a1c77a1b59c5fb066ee48f
Cloudbop <boppincolin@gmail.com>
2021-09-20 21:10:42 +0100

    +AllStaff passes->handle query error

--- client/src/components/staff/tests/AllStaff.test.tsx
+++ client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,10 +1,17 @@
-import { screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { QueryClientProvider, setLogger } from 'react-query';
+import { generateQueryClient } from 'react-query/queryClient';
 
-import { renderWithQueryClient } from '../../../test-utils/index';
-// import { rest } from 'msw';
 // import { defaultQueryClientOptions } from '../../../react-query/queryClient';
-// import { server } from '../../../mocks/server';
+import { server } from '../../../mocks/server';
+import { renderWithQueryClient } from '../../../test-utils/index';
 import { AllStaff } from '../AllStaff';
+
+/** - 
+ console.warn
+    [MSW] Warning: captured a request without a matching request handler:
+ */
 
 test('renders response from query', async () => {
   // wrap component with React-Query Provider
@@ -19,9 +26,33 @@ test('renders response from query', async () => {
 
 test('handles query error', async () => {
   // (re)set handler to return a 500 error for staff
-  // server.resetHandlers(
-  //   rest.get('http://localhost:3030/staff', (req, res, ctx) => {
-  //     return res(ctx.status(500));
-  //   }),
-  // );
+  server.resetHandlers(
+    rest.get('http://localhost:3030/staff', (req, res, ctx) => {
+      return res(ctx.status(500));
+    }),
+  );
+  // supress react-query erros
+  setLogger({
+    log: console.log,
+    warn: console.warn,
+    error: () => {
+      // swallow react-query erros without printing out
+    },
+  });
+
+  //
+  const queryClient = generateQueryClient();
+  const options = queryClient.getDefaultOptions();
+  // stop default retries
+  options.queries = { ...options.queries, retry: false };
+  queryClient.setDefaultOptions(options);
+
+  render(
+    <QueryClientProvider client={queryClient}>
+      <AllStaff />
+    </QueryClientProvider>,
+  );
+
+  const alertToast = await screen.findByRole('alert');
+  expect(alertToast).toHaveTextContent('Request failed with status code 500');
 });
