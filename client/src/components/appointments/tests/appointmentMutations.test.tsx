ea6a77c6941424d8dce7e7718dd623c1d0b54a8c
Cloudbop <boppincolin@gmail.com>
2021-09-20 21:20:50 +0100

    +test cancel appointment

--- client/src/components/appointments/tests/appointmentMutations.test.tsx
+++ client/src/components/appointments/tests/appointmentMutations.test.tsx
@@ -39,10 +39,26 @@ test('Reserve appointment', async () => {
   await waitForElementToBeRemoved(alertToast);
 });
 
-// test('Cancel appointment', async () => {
-// your test here
-//
-// const cancelButtons = await screen.findAllByRole('button', {
-//   name: /cancel appointment/i,
-//  });
-// });
+test('Cancel appointment', async () => {
+  renderWithQueryClient(
+    <MemoryRouter>
+      <Calendar />
+    </MemoryRouter>,
+  );
+  // find all the cancel buttons
+  const cancelButtons = await screen.findAllByRole('button', {
+    name: /cancel appointment/i,
+  });
+
+  // click on the first one to reserve
+  fireEvent.click(cancelButtons[0]);
+
+  // check for the toast alert
+  const alertToast = await screen.findByRole('alert');
+  expect(alertToast).toHaveTextContent('cancel');
+
+  // close alert to keep state clean and wait for element to disappear
+  const alertCloseButton = screen.getByRole('button', { name: 'Close' });
+  alertCloseButton.click();
+  await waitForElementToBeRemoved(alertToast);
+});
