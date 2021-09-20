4431c33d6591b01011be38ea9b80c8bc82a0f3b8
Cloudbop <boppincolin@gmail.com>
2021-09-20 21:33:53 +0100

    +config testing custom hooks

--- client/src/test-utils/index.tsx
+++ client/src/test-utils/index.tsx
@@ -34,9 +34,9 @@ export function renderWithQueryClient(
 
 // FOR TESTING CUSTOM HOOKS
 // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
-// export const createQueryClientWrapper = () => {
-//   const queryClient = generateQueryClient();
-//   return ({ children }) => (
-//     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-//   );
-// };
+export const createQueryClientWrapper = () => {
+  const queryClient = generateQueryClient();
+  return ({ children }) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
