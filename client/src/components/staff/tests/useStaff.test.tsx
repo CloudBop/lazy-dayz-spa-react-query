26b079af975f3c1c1f4c25d68c05446c6e32433e
Cloudbop <boppincolin@gmail.com>
2021-09-20 21:42:55 +0100

    +useStaff filter by service

--- client/src/components/staff/tests/useStaff.test.tsx
+++ client/src/components/staff/tests/useStaff.test.tsx
@@ -1,5 +1,20 @@
+import { act } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
+import { createQueryClientWrapper } from 'test-utils';
+
 import { useStaff } from '../hooks/useStaff';
 
 test('filter staff', async () => {
-  // the magic happens here
+  const { result, waitFor } = renderHook(useStaff, {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  // wait for staff to populate
+  await waitFor(() => result.current.staff.length === 4);
+
+  // set to filter only staff who give scrub
+  act(() => result.current.setFilter('scrub'));
+
+  // wait for staff list state to update, should be 3
+  await waitFor(() => result.current.staff.length === 2);
 });
